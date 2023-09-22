4e445ba8732d3a946928391d5c625603ea1b990b
JWilhelm <jan.wilhelm@ur.de>
2023-09-22 13:22:46 +0200

    compute sum of Mulliken charges in tip and substrate

--- compute_classical_dipole.py
+++ compute_classical_dipole.py
@@ -1,38 +1,50 @@
 import os
 
 # Define the directory and file path
-directory_path = "../01_dist_10A"
+directory_path = "../../01_dist_10A"
 file_name = "cp2k.out"
 file_path = os.path.join(directory_path, file_name)
 
-# Function to read lines from 3rd to 1013th following "Mulliken"
-def read_lines_after_mulliken(file_path, start_line=3, num_lines=1011):
-    found_mulliken = False
-    result = []
-
+# Function to read lines from 3rd to 1011 following "Mulliken" and perform calculations
+def process_mulliken_data(file_path):
     try:
         with open(file_path, 'r') as file:
             lines = file.readlines()
-            for i, line in enumerate(lines):
-                if "Mulliken" in line:
-                    found_mulliken = True
-                    result.extend(lines[i + start_line - 1:i + start_line + num_lines - 1])
-                    break
+
+        # Initialize variables to store the sums
+        sum1_to_351 = 0.0
+        sum352_to_1010 = 0.0
+
+        # Flag to indicate when to start reading lines
+        read_lines = False
+
+        for line in lines:
+            if "Mulliken" in line:
+
+                print(f"Sum of entries 1 to 351: {sum1_to_351}")
+                print(f"Sum of entries 352 to 1010: {sum352_to_1010}")
+
+                # Reset sums when a new "Mulliken" occurrence is found
+                sum1_to_351 = 0.0
+                sum352_to_1010 = 0.0
+                read_lines = True
+                i = 0
+            elif read_lines:
+                i += 1
+                # Extract the 5th entry from the line
+                entries = line.split()
+                if len(entries) >= 5 and i > 2 and i < 1013:
+                    entry_5 = float(entries[4])
+
+                    # Update sums based on entry position
+                    if i >= 353:
+                        sum1_to_351 += entry_5
+                    else:
+                        sum352_to_1010 += entry_5
 
     except FileNotFoundError:
         print(f"File '{file_path}' not found.")
-        return None
+        return
 
-    if found_mulliken:
-        return result
-    else:
-        print("Mulliken not found in the file.")
-        return None
-
-# Call the function to read lines
-mulliken_lines = read_lines_after_mulliken(file_path)
-
-# Print the lines if found
-if mulliken_lines:
-    for line in mulliken_lines:
-        print(line.strip())
+# Call the function to process Mulliken data
+process_mulliken_data(file_path)
